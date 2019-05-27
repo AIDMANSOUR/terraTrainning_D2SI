b33ccbdba1b9e857f2fbeeced77271d8c5a56c67
aidmansour <aliidmansour0@gmail.com>
2019-05-28 00:09:18 +0200

    VPC and Subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,3 +2,24 @@
 provider "aws" {
   region = "${var.aws_region}"
 }
+
+###### VPC ######
+resource "aws_vpc" "labs1" {
+  cidr_block       = "172.23.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags = {
+    Name = "labs1"
+  }
+}
+
+###### Subnet ######
+resource "aws_subnet" "web-public-2a" {
+  cidr_block        = "172.23.1.0/24"
+  availability_zone = "${var.aws_region}a"
+  vpc_id            = "${aws_vpc.labs1.id}"
+
+  tags {
+    Name = "Labs1"
+  }
+}
