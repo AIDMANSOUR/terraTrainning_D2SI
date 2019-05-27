453722388d5a2565e07f3373a70bf48d3cbcf4b8
aidmansour <aliidmansour0@gmail.com>
2019-05-28 01:35:53 +0200

    Fin VPC

--- vpc/main.tf
+++ vpc/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 ###### VPC ######
-resource "aws_vpc" "labs1" {
+resource "aws_vpc" "labs1_vpc" {
   cidr_block       = "172.23.0.0/16"
   instance_tenancy = "dedicated"
 
@@ -14,12 +14,41 @@ resource "aws_vpc" "labs1" {
 }
 
 ###### Subnet ######
-resource "aws_subnet" "web-public-2a" {
+resource "aws_subnet" "labs1_public" {
   cidr_block        = "172.23.1.0/24"
   availability_zone = "${var.aws_region}a"
-  vpc_id            = "${aws_vpc.labs1.id}"
+  vpc_id            = "${aws_vpc.labs1_vpc.id}"
 
   tags {
     Name = "Labs1"
   }
+}
+
+###### IGW ######
+resource "aws_internet_gateway" "labs1_igw" {
+  vpc_id = "${aws_vpc.labs1_vpc.id}"
+
+  tags = {
+    Name = "Labs1"
+  }
+}
+
+###### Route Table ######
+resource "aws_route_table" "labs1_rt" {
+  vpc_id = "${aws_vpc.labs1_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.labs1_igw.id}"
+  }
+
+  tags {
+    Name = "labs1"
+  }
+}
+
+###### Route table association with public subnets ######
+resource "aws_route_table_association" "labs1_art" {
+  subnet_id      = "${aws_subnet.labs1_public.id}"
+  route_table_id = "${aws_route_table.labs1_rt.id}"
 }
